1ad5e7820e03162310366ca24e8567ebc98a2fb5
David Wheatley <hi@davwheat.dev>
2020-09-11 17:51:50 +0100

    Escape string in URL

--- Constants.js
+++ Constants.js
@@ -3,7 +3,7 @@ const env = process.env;
 module.exports = Object.freeze({
   API_VERSION: 'preview',
   CACHE_TTL: 1000 * 60 * 5,
-  GIT_RAW_URL: `https://raw.githubusercontent.com/${env.REPO_NAME}/master`,
+  GIT_RAW_URL: `https://raw.githubusercontent.com/${encodeURI(env.REPO_NAME)}/master`,
   CACHE_ENABLED: true,
   CDN_URL: `https://msfs-liverypack-cdn.mrproper.dev`,
 });
