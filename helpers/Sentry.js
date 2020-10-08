86d75433e32cb9bd320fd04f3d6ff90bd4e84e02
Jaap <jaap@mrproper.dev>
2020-10-08 19:29:27 +0200

    API update for images

--- helpers/Sentry.js
+++ helpers/Sentry.js
@@ -1,9 +1,9 @@
-const Sentry = require("@sentry/node");
+const Sentry = require('@sentry/node');
 require('dotenv').config();
 // or use es6 import statements
 // import * as Sentry from '@sentry/node';
 
-const Tracing = require("@sentry/tracing");
+const Tracing = require('@sentry/tracing');
 // or use es6 import statements
 // import * as Tracing from '@sentry/tracing';
 
