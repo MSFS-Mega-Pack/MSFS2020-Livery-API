68dd8387394aacc98e57763ff9eed04d31cd0d91
Jaap <jaap@mrproper.dev>
2020-10-09 14:16:48 +0200

    Add try catch to upload, to prevent time out and crashes

--- livery-store/cloud-storage.js
+++ livery-store/cloud-storage.js
@@ -132,24 +132,28 @@ async function getThumbnail(liveryType, liveryName, sum) {
 }
 
 async function uploadFile(sourceDirectory, metadata, Destdirectory) {
-  await getMetadata(Destdirectory);
+  //await getMetadata(Destdirectory);
   // Uploads a local file to the bucket
-  await storage.bucket(bucketName).upload(sourceDirectory, {
-    destination: Destdirectory,
-    // Support for HTTP requests made with `Accept-Encoding: gzip`
-    gzip: true,
-    // By setting the option `destination`, you can change the name of the
-    // object you are uploading to a bucket.
-    metadata: {
-      // Enable long-lived HTTP caching headers
-      // Use only if the contents of the file will never change
-      // (If the contents will change, use cacheControl: 'no-cache')
-      cacheControl: 'public, max-age=31536000',
-      metadata,
-    },
-  });
+  try {
+    await storage.bucket(bucketName).upload(sourceDirectory, {
+      destination: Destdirectory,
+      // Support for HTTP requests made with `Accept-Encoding: gzip`
+      gzip: true,
+      // By setting the option `destination`, you can change the name of the
+      // object you are uploading to a bucket.
+      metadata: {
+        // Enable long-lived HTTP caching headers
+        // Use only if the contents of the file will never change
+        // (If the contents will change, use cacheControl: 'no-cache')
+        cacheControl: 'public, max-age=31536000',
+        metadata,
+      },
+    });
 
-  console.log(`${sourceDirectory} uploaded to ${Destdirectory}.`);
+    console.log(`${sourceDirectory} uploaded to ${Destdirectory}.`);
+  } catch (error) {
+    console.log(`Uploading failed for: ${sourceDirectory}\nReason:`, error);
+  }
 }
 
 async function getMetadata(filename) {
