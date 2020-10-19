b3c38d37c3b5aa163fc570d9d106494ff40ef2c8
davwheat <hi@davwheat.dev>
2020-10-19 17:01:14 +0100

    Implement version constraint from Clink's CDN

--- data/updates/GetLatestVersion.js
+++ data/updates/GetLatestVersion.js
@@ -4,6 +4,7 @@ const Log = require('../../logger');
 module.exports = GetLatestVersion;
 
 async function GetLatestVersion() {
+  const maxVersionRes = await fetch('https://liveriesmegapack.b-cdn.net/current_version.txt');
   const res = await fetch(`https://api.github.com/repos/MSFS-Mega-Pack/MSFS2020-livery-manager/releases/latest`);
 
   let res1 = res.clone();
@@ -13,9 +14,21 @@ async function GetLatestVersion() {
     return null;
   }
 
+  let maxVersion = '9999.0.0';
+
+  if (maxVersionRes.status !== 200) {
+    Log(
+      "Failed to fetch latest allowed version from Clink's CDN. We will assume that we can update to the latest version.",
+      Log.SEVERITY.WARNING
+    );
+  } else {
+    maxVersion = await maxVersionRes.text();
+  }
+
   const jsonRelease = await res1.json();
 
   return {
+    latestAllowed: maxVersion,
     latest: jsonRelease.tag_name.substr(1), // get version without "v" at the start
     downloadUrl: jsonRelease.assets.filter(asset => asset.name.endsWith('.exe'))[0].browser_download_url,
     date: jsonRelease.published_at,
